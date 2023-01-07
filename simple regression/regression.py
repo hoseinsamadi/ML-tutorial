3b6962c8da719b9dcab13a552cd30b0ba1b3a33b
alirezakarimi92 <kalireza144@gmail.com>
2023-01-07 21:50:19 +0330

    make primary folders of project

---
+++ simple regression/regression.py
@@ -0,0 +1,7 @@
+# here we start to create some code in regression
+
+# for every ML code we need 4 Item:
+# 1. the most importatn item: a Data set 
+# 2. a hypothesis which is a parametric function & our task is finding its parameters
+# 3. a cost function which make a relation between "Data Set" & "Parameters of hypothesis"
+# 4. an optimization method which optimize the cost function
